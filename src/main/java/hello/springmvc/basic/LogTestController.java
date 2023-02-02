60edce231b56e71aa9168e5eec1a8210734094ae
core <jinjs6@gmail.com>
2023-02-02 18:12:12 +0900

    feat: 로그 테스트 컨트롤러 파일 생성 및 로그 테스트 코드 작성

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass()); //LogTestController
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.info(" info log = {}", name);
+        log.trace(" trace log = {}", name);
+        log.error(" error log = {}", name);
+        log.debug(" debug log = {}", name);
+        log.warn(" warn log = {}", name);
+
+        return "ok";
+    }
+}
