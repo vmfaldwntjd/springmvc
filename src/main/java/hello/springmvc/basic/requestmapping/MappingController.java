6ea0255a16392cf8d1a4669465883b2c76524e5f
core <jinjs6@gmail.com>
2023-02-02 19:57:37 +0900

    feat: PathVariable 다중 매핑 적용 메서드 작성

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -51,4 +51,15 @@ public class MappingController {
         log.info("userId = {}", data);
         return "ok";
     }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long
+            orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
 }
