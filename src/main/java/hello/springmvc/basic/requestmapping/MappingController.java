415aa0547b9b9d6717496d4d59e19642f437e03d
core <jinjs6@gmail.com>
2023-02-02 20:00:18 +0900

    feat: PathVariable 다중 매핑 적용에서 특정 파라미터 조건 추가 기능 작성

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -62,4 +62,17 @@ public class MappingController {
         return "ok";
     }
 
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug" (! = )
+     * params = {"mode=debug","data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
 }
