3ef6d45b4c21893936cd8f66642673a7f48f9396
core <jinjs6@gmail.com>
2023-02-03 12:40:03 +0900

    feat: requestBodyJsonV3 메서드 작성

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -47,4 +47,11 @@ public class RequestBodyJsonController {
 
         return "ok";
     }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
 }
