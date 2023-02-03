3b7deba0f04fbafac9cb1ee74503167b493cd78f
core <jinjs6@gmail.com>
2023-02-03 13:45:28 +0900

    feat: ResponseBodyController 파일 생성, responseBodyV1 메서드 작성

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,18 @@
+package hello.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response/body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+}
