488737c80c992281be9c12c2dca83ab87d7ccc70
core <jinjs6@gmail.com>
2023-02-03 11:57:15 +0900

    feat: RequestBodyStringController 파일 생성

---
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -0,0 +1,12 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+
+}
